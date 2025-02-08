e0a22ad3cec63639f935a0d2439a6a5f31f7d3aa
Syedsaad40 <mosyedsaad11@gmail.com>
2025-02-08 16:32:17 +0500

    2.cpp

--- OOP-LAB-2/2.cpp
+++ OOP-LAB-2/2.cpp
@@ -1,16 +1,21 @@
 #include<iostream>
 using namespace std;
 
-void* add(void* a){
-	int* ab=(int*)a;
-	return a;
+void* add(void* a, int value) {
+    int* arr = (int*)a;
+    for (int i = 0; i < 5; i++) {
+        arr[i] += value;
+    }
+    return a;
 }
-int main(){
-	int arr[5]={1,2,3,4,5};
-	int *ptr=(int*)add((void*)&arr);
-	for(int i=0;i<5;i++){
-		cout<<ptr[i]+2<<" ";
-	}
-		return 0;   
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int* ptr = (int*)add((void*)arr, 2);
+    
+    for (int i = 0; i < 5; i++) {
+        cout << ptr[i] << " ";
+    }
+    
+    return 0;
 }
-	
